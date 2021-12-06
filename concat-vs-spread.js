2d488247d5cd1c57a22e5f03aa6a2a2a6120f7cf
loktysh <49595107+Loktysh@users.noreply.github.com>
2021-12-06 16:07:22 +0300

    feat: add Array.form vs Split benchmark for stirng to array

--- concat-vs-spread.js
+++ concat-vs-spread.js
@@ -6,3 +6,12 @@ console.timeEnd('Concat');
 console.time('Spread');
 a4 = [...a1, ...a2]
 console.timeEnd('Spread');
+
+a1 = [...new Array(10000000)].map((e)=>e =  Math.round(Math.random() * (126 - 33)) + 33)
+s1 = a1.map((e)=>String.fromCharCode(e)).join('')
+console.time('Array.from');
+Array.from(s1)
+console.timeEnd('Array.from');
+console.time('Split');
+s1.split('')
+console.timeEnd('Split');
